954546bfa59c065ccc6a3e6650132b5fa4e65a50
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2023-02-03 19:18:16 +0600

    design shape

--- Components/Programs/Programs.jsx
+++ Components/Programs/Programs.jsx
@@ -1,8 +1,14 @@
 import Image from "next/image";
 import React from "react";
-import Logo from "../../Assets/Images/logo2.svg";
-import gallery from "../../Assets/Images/gallery.svg";
-import shape from "../../Assets/Images/shape.svg";
+import round1 from "../../Assets/Images/Rectangle2.png";
+import round2 from "../../Assets/Images/Rectangle1.png";
+import round3 from "../../Assets/Images/Rectangle3.png";
+import group from "../../Assets/Images/Group.svg";
+import group1 from "../../Assets/Images/Group (1).svg";
+import group2 from "../../Assets/Images/Group (2).svg";
+import group3 from "../../Assets/Images/Group (3).svg";
+import group4 from "../../Assets/Images/Group (4).svg";
+import group5 from "../../Assets/Images/Group (5).svg";
 
 const Programs = () => {
 
@@ -13,18 +19,74 @@ const Programs = () => {
                     <div className="w-full md:w-1/3 mx-auto text-center text-3xl md:text-5xl">
                         <h2 style={{ fontFamily: "SportingGrotesque" }} className="">Programs that can be taken</h2>
                     </div>
-                    <div className={`flex flex-col md:flex-row justify-around items-center p-2 p-md-5 w-full md:w-4/5 mx-auto py-20`}>
+                    <div className={`grid grid-cols-1 md:grid-cols-2 lg:grid-cls-3 justify-around items-center p-2 p-md-5 w-full md:w-4/5 mx-auto py-20`}>
                         <div className="flex flex-row justify-around items-center p-3 text-left gap-1 w-full">
-                            <div className="w-1/3">
-                                <div className="bg-[#E275CD] w-16 h-16 rounded-2xl">
-
+                            <div className="w-1/3 items-center justify-center flex">
+                                <div className="absolute">
+                                    <Image
+                                        className="w-24 h-24 p-5"
+                                        src={group}
+                                        alt="logo"
+                                        title="logo"
+                                    />
                                 </div>
+                                <Image
+                                    className="w-32 h-32"
+                                    src={round1}
+                                    alt="logo"
+                                    title="logo"
+                                />
                             </div>
                             <div className="w-2/3 space-y-2">
-                                <h2 className="font-bold">Suketi Mantapo</h2>
-                                <p className="text-gray-500">Designer Graphic</p>
+                                <h2 className="font-bold">Gets ABS in 2 weeks</h2>
+                                <p className="text-gray-500">Exercise is a very important need for our body. </p>
                             </div>
                         </div>
+                        <div className="flex flex-row justify-around items-center p-3 text-left gap-1 w-full">
+                            <div className="w-1/3 items-center justify-center flex">
+                                <div className="absolute">
+                                    <Image
+                                        className="w-24 h-24 p-5"
+                                        src={group1}
+                                        alt="logo"
+                                        title="logo"
+                                    />
+                                </div>
+                                <Image
+                                    className="w-32 h-32"
+                                    src={round2}
+                                    alt="logo"
+                                    title="logo"
+                                />
+                            </div>
+                            <div className="w-2/3 space-y-2">
+                                <h2 className="font-bold">Gets ABS in 2 weeks</h2>
+                                <p className="text-gray-500">Exercise is a very important need for our body. </p>
+                            </div>
+                        </div>
+                        <div className="flex flex-row justify-around items-center p-3 text-left gap-1 w-full">
+                            <div className="w-1/3 items-center justify-center flex">
+                                <div className="absolute">
+                                    <Image
+                                        className="w-24 h-24 p-5"
+                                        src={group2}
+                                        alt="logo"
+                                        title="logo"
+                                    />
+                                </div>
+                                <Image
+                                    className="w-32 h-32"
+                                    src={round3}
+                                    alt="logo"
+                                    title="logo"
+                                />
+                            </div>
+                            <div className="w-2/3 space-y-2">
+                                <h2 className="font-bold">Gets ABS in 2 weeks</h2>
+                                <p className="text-gray-500">Exercise is a very important need for our body. </p>
+                            </div>
+                        </div>
+
                     </div>
                 </div>
             </section>
