b0a0d6ba88130d32a8aac72317d7d73706bef073
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2023-02-03 15:18:42 +0600

    nav item add

--- Components/Header/Navbar.jsx
+++ Components/Header/Navbar.jsx
@@ -12,22 +12,29 @@ const Navbar = () => {
             <div className="container py-6 px-4">
                 <nav>
                     <div className=" flex flex-row justify-between">
-                        {/* <div className=" flex space-x-3 items-center">
-                            <Link href="/" scroll legacyBehavior>
-                                <Image src={logo}/>
-                            </Link>
-                        </div> */}
+                        <div className="">
+                        <img className="h-32 w-44" src={logo} alt="" />
+                        </div>
                         {/* For large (i.e. desktop and laptop sized screen) */}
                         <div className="lg:flex hidden flex-auto justify-between flex-row">
 
                             <div className="flex flex-row lg:space-x-3 xl:space-x-4">
-                                <button className="rounded-md flex space-x-2 w-24 h-10 font-normal text-sm leading-3 text-gray-800 bg-white focus:outline-none focus:bg-gray-200 hover:bg-gray-200 duration-150 justify-center items-center">
-                                    <p>Share</p>
+                                <button className="rounded-md flex space-x-2 w-24 h-10 font-normal text-sm leading-3 text-gray-800 focus:outline-none focus:bg-gray-200 hover:bg-gray-200 duration-150 justify-center items-center">
+                                    <p>Home</p>
+                                </button>
+                                <button className="rounded-md flex space-x-2 w-24 h-10 font-normal text-sm leading-3 text-gray-800 focus:outline-none focus:bg-gray-200 hover:bg-gray-200 duration-150 justify-center items-center">
+                                    <p>Program</p>
+                                </button>
+                                <button className="rounded-md flex space-x-2 w-24 h-10 font-normal text-sm leading-3 text-gray-800 focus:outline-none focus:bg-gray-200 hover:bg-gray-200 duration-150 justify-center items-center">
+                                    <p>Blog</p>
+                                </button>
+                                <button className="rounded-md flex space-x-2 w-24 h-10 font-normal text-sm leading-3 text-gray-800 focus:outline-none focus:bg-gray-200 hover:bg-gray-200 duration-150 justify-center items-center">
+                                    <p>About US</p>
                                 </button>
                             </div>
                         </div>
                         <div className=" hidden sm:flex xl:pl-4 lg:pl-3 justify-end flex-row sm:space-x-4 md:space-x-6 lg:space-x-3 xl:space-x-4">
-                            <button className="rounded-md flex space-x-2 w-24 h-10 font-normal text-sm leading-3 text-gray-800 bg-[#264373] focus:outline-none focus:bg-gray-200 hover:bg-gray-200 duration-150 justify-center items-center">
+                            <button className="rounded-md flex space-x-2 w-24 h-10 font-normal text-sm leading-3 text-white bg-[#264373] hover:text-black focus:outline-none focus:bg-gray-200 hover:bg-gray-200 duration-150 justify-center items-center">
                                 <p>Login</p>
                             </button>
 
